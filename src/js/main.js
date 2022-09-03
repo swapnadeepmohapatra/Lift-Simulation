f19d21deb6e9eeb553ef5a7523ad6af3f3bd039a
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2022-09-03 10:34:03 +0530

    feat: milestone 2 done

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,30 @@
-function LiftSimulator(el, floors, lifts) {
-  const state = {
-    lifts: new Array(lifts).fill(0).map((_, i) => ({
-      busy: false,
-      floor: 0,
-      index: i,
-    })),
-  };
+function LiftSimulator(el) {
+  let floors;
+  let state;
 
-  renderElemenets(el, state);
-  addEventListeners(el);
+  function start(_floors, _lifts) {
+    floors = _floors;
+    state = {
+      lifts: new Array(_lifts).fill(0).map((_, i) => ({
+        busy: false,
+        floor: 0,
+        index: i,
+      })),
+    };
+
+    removeEventListeners(el);
+    renderElemenets(el, state);
+    addEventListeners(el);
+  }
 
   function renderElemenets(el, state) {
     const createdLifts = state.lifts
       .map((_, index) => {
         return `<div class="lift" id="lift${index}" data-current="${0}" style="margin-top: ${
           (floors - 1) * 116
-        }px; margin-left: ${(index + 1) * 100}px"/></div>`;
+        }px; margin-left: ${
+          (index + 1) * 100
+        }px"/><div class="door1"></div><div class="door2"></div></div>`;
       })
       .join("");
 
@@ -42,36 +51,108 @@ function LiftSimulator(el, floors, lifts) {
   }
 
   function addEventListeners(el) {
-    el.addEventListener("click", (e) => {
-      if (e.target.id === "up" || e.target.id === "down") {
-        callLift(e.target.dataset.floor);
-      }
-
-      console.log(state);
-    });
+    el.addEventListener("click", buttonClickEvent, true);
   }
 
+  function removeEventListeners(el) {
+    el.removeEventListener("click", buttonClickEvent, true);
+  }
+
+  const buttonClickEvent = (e) => {
+    if (e.target.id === "up" || e.target.id === "down") {
+      callLift(e.target.dataset.floor);
+    }
+  };
+
   function callLift(floor) {
-    moveLift(0, state.lifts[0].floor, floor);
+    const closestLift = getClosestEmptyLift(floor);
+    moveLift(closestLift, state.lifts[closestLift].floor, floor);
   }
 
   function moveLift(lift_no, currentFloor, newFloor) {
     if (state.lifts[lift_no].busy === true) return;
 
     const lift = document.getElementById(`lift${lift_no}`);
-    console.log(newFloor - currentFloor);
     lift.style.transform = `translateY(${-116 * newFloor}px)`;
     lift.style.transition = `transform ${
       2000 * Math.abs(newFloor - currentFloor)
     }ms ease-in-out`;
 
+    if (newFloor > currentFloor) {
+      lift.classList.add("lift-up");
+    } else if (newFloor < currentFloor) {
+      lift.classList.add("lift-down");
+    }
+
     state.lifts[lift_no].floor = Number(newFloor);
     state.lifts[lift_no].busy = true;
 
     setTimeout(() => {
-      state.lifts[lift_no].busy = false;
+      openLift(lift);
     }, 2000 * Math.abs(newFloor - currentFloor));
+
+    setTimeout(() => {
+      state.lifts[lift_no].busy = false;
+    }, 2000 * Math.abs(newFloor - currentFloor) + 5001);
   }
+
+  function getClosestEmptyLift(destFloor) {
+    const emptyLifts = state.lifts.reduce(
+      (result, value) =>
+        result.concat(
+          value.busy === false
+            ? {
+                ...value,
+                distance: Math.abs(destFloor - value.floor),
+              }
+            : []
+        ),
+      []
+    );
+
+    if (emptyLifts.length <= 0) {
+      return null;
+    }
+
+    const closestLift = emptyLifts.reduce((result, value) =>
+      value.distance < result.distance ? value : result
+    );
+
+    return closestLift.index;
+  }
+
+  function openLift(lift) {
+    const door1 = lift.childNodes[1];
+    door1.classList.add("open");
+
+    const door2 = lift.childNodes[0];
+    door2.classList.add("open");
+
+    lift.classList.remove("lift-up");
+    lift.classList.remove("lift-down");
+
+    setTimeout(() => {
+      door1.classList.remove("open");
+      door1.classList.add("close");
+      door2.classList.remove("open");
+      door2.classList.add("close");
+    }, 2500);
+
+    setTimeout(() => {
+      door1.classList.remove("close");
+      door2.classList.remove("close");
+    }, 5000);
+  }
+
+  return {
+    start,
+  };
 }
 
-LiftSimulator(document.getElementById("app"), 6, 3);
+const liftsim = LiftSimulator(document.getElementById("app"));
+document.querySelector("button").addEventListener("click", () => {
+  liftsim.start(
+    Number(document.getElementById("floors").value),
+    Number(document.getElementById("lifts").value)
+  );
+});
